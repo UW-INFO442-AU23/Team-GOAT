0381bcf5ac6d78c296ea2b4ddd4352a19f3dea00
joyce3kim <jkimy@uw.edu>
2023-10-23 13:47:52 -0700

    components file made

---
+++ src/components/About/about.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlR6EUdSNXf94mjqOyfMF8SKougIS5xy0",
+  authDomain: "team-goat-ced05.firebaseapp.com",
+  projectId: "team-goat-ced05",
+  storageBucket: "team-goat-ced05.appspot.com",
+  messagingSenderId: "1023525263496",
+  appId: "1:1023525263496:web:aa4887cb7f2ef4093aa73d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
