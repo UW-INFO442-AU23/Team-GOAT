863f12614baca6a85bbae8a9e66032526c424647
joyce <jkimy@uw.edu>
2023-11-08 11:14:49 -0800

    added footer to about and resource page

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,1 +1,23 @@
+import React from "react";
 
+function About() {
+
+    return (
+        <section>
+
+            <div className="container">
+                <h1>Campus Map</h1>
+                <p className="text-white lead">Lorem Ipsum is simply dummy text of the printing industry.</p>
+            </div>
+
+            <footer>
+                <div>
+                    <p>&copy; CampusCare 2023 INFO 442 Team GOAT</p>
+                </div>
+            </footer>
+
+        </section>
+      )
+}
+
+export default About;
--- src/components/Resources/resources.js
+++ src/components/Resources/resources.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Resources = () => {
-	return (
-        <>
-        
-        </>
-    )
-    }
+function Resources() {
 
-    export default Resources;
+    return (
+        <section>
+
+            <div className="container">
+                <h1>Campus Map</h1>
+                <p className="text-white lead">Lorem Ipsum is simply dummy text of the printing industry.</p>
+            </div>
+
+            <footer>
+                <div>
+                    <p>&copy; CampusCare 2023 INFO 442 Team GOAT</p>
+                </div>
+            </footer>
+
+        </section>
+      )
+}
+
+export default Resources;
