7bbb0289dee8d79242c0f7bd1f0c23909ecd13fb
tylertakeuchi <102692670+tylertakeuchi@users.noreply.github.com>
2023-11-07 17:50:23 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import About from './components/About/about.js';
-import Home from './components/Home/home.js'
-// import Resources from './components/resources.js'
+import About from './components/About/about';
+import Home from './components/Home/home'
+import Navbar from './components/Navbar/navbar';
+import Tag from './components/Tag/tag';
+import Footer from './components/Footer/footer';
+import Resources from './components/Resources/resources';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App(props) {
@@ -11,7 +14,10 @@ export default function App(props) {
           <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/About" element={<About />} />
-  
+          <Route path="/Tag" element={<Tag />} />
+          <Route path="/Navbar" element={<Navbar />} />
+          <Route path="/Footer" element={<Footer />} />
+          <Route path="/Resources" element={<Resources />} />
           </Routes>
         </Router> 
       </div>
