cba91f9dd3c11b6dfe03c603fea621d90a44f519
joyce <jkimy@uw.edu>
2023-11-08 14:45:20 -0800

    js and css for about page

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,24 +1,24 @@
 import React from "react";
 import './about.css'
 
-function About() {
-
+export default function About() {
     return (
         <section>
-
-            <div className="container">
-                <h1>Campus Map</h1>
-                <p className="text-white lead">Lorem Ipsum is simply dummy text of the printing industry.</p>
+            <h1 className="about-h1">Welcome to Campus Care!</h1>
+            <h2 className="about-h2">Resource for finding menstrual products on UW Seattle Campus</h2>
+            <div className="about-paragraph">
+                <p>Mission Statement
+                </p>
+                <p>Meet Our Team
+                </p>
+                <p>About Us
+                </p>
             </div>
-
             <footer>
                 <div>
                     <p>&copy; CampusCare 2023 INFO 442 Team GOAT</p>
                 </div>
             </footer>
-
         </section>
-      )
+    )
 }
-
-export default About;
